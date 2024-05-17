0f392615a0db667945afcbf525e81cbe61051f49
Lara Bernardes <contatolarabernardes@gmail.com>
2024-05-17 13:53:49 +0300

    Add: added student and course in model

---
+++ src/main/java/lv/venta/model/Course.java
@@ -0,0 +1,56 @@
+package lv.venta.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table(name = "Course table")
+@Entity
+
+public class Course {
+	@Setter(value = AccessLevel.NONE)
+	@Column(name = "Idc")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long idc;
+	
+	@NotNull
+	@Pattern(regexp = "[A-Z]{1}[a-z]+")
+	@Size(min = 2, max = 10)
+	@Column(name = "Title")
+	private String title;
+	
+	@Column(name = "Credit points")
+	@Min(2)
+	@Max(20)
+	private int creditPoints;
+	
+	@NotNull
+	@Column(name = "Professor")
+	private Professor professor;
+	
+	public Course(String Title, int creditPoints, Professor professor) {
+		setTitle(title);
+		setCreditPoints(creditPoints);
+		
+	}
+	
+	
+}
---
+++ src/main/java/lv/venta/model/Student.java
@@ -0,0 +1,50 @@
+package lv.venta.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table(name = "Student table")
+@Entity
+
+public class Student {
+	@Setter(value = AccessLevel.NONE)
+	@Column(name = "Ids")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long ids;
+	
+	@NotNull
+	@Pattern(regexp = "[A-Z]{1}[a-z]+")
+	@Size(min = 2, max = 10)
+	@Column(name = "Name")
+	private String name;
+	
+	@NotNull
+	@Pattern(regexp = "[A-Z]{1}[a-z]+")
+	@Size(min = 2, max = 20)
+	@Column(name = "Surname")
+	private String surname;
+	
+	public Student(String name, String surname) {
+		setName(name);
+		setSurname(surname);
+	}
+	
+	
+}
