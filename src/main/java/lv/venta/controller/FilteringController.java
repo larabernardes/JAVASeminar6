7adabae71e9b4d2cfa235d2f718c9109e7627c73
Lara Bernardes <contatolarabernardes@gmail.com>
2024-05-24 16:07:13 +0300

    Add: added html that shows all grades

---
+++ src/main/java/lv/venta/controller/FilteringController.java
@@ -0,0 +1,36 @@
+package lv.venta.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lv.venta.model.Grade;
+import lv.venta.service.IFilteringService;
+
+@Controller
+@RequestMapping("/filter")
+public class FilteringController {
+	
+	@Autowired
+	private IFilteringService filterService;
+	
+	@GetMapping("/grade/failed") //localhost:8080/filter/grade/failed
+	public  String getFilterGradeFailed(Model model) {
+		
+		try
+		{
+			ArrayList<Grade> dataFromService= filterService.selectFailedGradesInSystem();
+			model.addAttribute("myPackage", dataFromService);
+			
+			return "show-all-grades-page";
+		}
+		catch (Exception e) {
+			return "error-page";
+		}
+	}
+
+}
